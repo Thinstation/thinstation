e93d9717c9d46e735dfcaf2bae3284f94f626653
Donald A. Cupp Jr <doncuppjr@yahoo.com>
2014-01-21 21:18:07 -0800

    Finish updating the xorg7_nvidia package 	modified:   ts/5.1/packages/xorg7_nvidia/build/extra/etc/udev/scripts/nvidia.sh 	modified:   ts/5.1/packages/xorg7_nvidia/build/install

--- ts/5.1/packages/xorg7_nvidia/build/extra/etc/udev/scripts/nvidia.sh
+++ ts/5.1/packages/xorg7_nvidia/build/extra/etc/udev/scripts/nvidia.sh
@@ -1,15 +1,12 @@
 #! /bin/sh
 
-NVGLVER=`echo /lib/nvidia/libGL.so.* | cut -d. -f3-4`
+NVGLVER=`find /lib -name libGL.so.\*.\* | cut -d. -f3-`
 
 rm -f /lib/libGL.so.1
-ln -s nvidia/libGL.so.$NVGLVER /lib/libGL.so.1
+ln -s libGL.so.$NVGLVER /lib/libGL.so.1
 
 rm -f /lib/xorg/modules/extensions/libglx.so
 ln -s libglx.so.$NVGLVER /lib/xorg/modules/extensions/libglx.so
-
-rm -f /lib/xorg/modules/libwfb.so
-ln -s libnvidia-wfb.so.1 /lib/xorg/modules/libwfb.so
 
 cat <<EOF > /etc/X11/xorg.conf.d/20-nvidia.conf
 Section "Device"
