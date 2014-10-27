15bfad91fb2cfaf2af37347a9b8f35b1b5b50589
Doncuppjr <doncuppjr@yahoo.com>
2014-10-27 08:51:59 -0700

    Fixes for autonet. 	modified:   ts/5.2/packages/autonet/etc/udev/scripts/net.sh 	modified:   ts/5.2/packages/base/etc/thinstation.defaults

--- ts/5.2/packages/autonet/etc/udev/scripts/net.sh
+++ ts/5.2/packages/autonet/etc/udev/scripts/net.sh
@@ -131,87 +131,85 @@ _wlan()
 		echo_log "The NET_USE value does not allow use of $INTERFACE"
 		exit
 	fi
-	if [ "$WIRELESS_ESSID" == "" ]; then
-		echo_log "No ESSID specified"
-		exit
-	fi
-	if [ "$WIRELESS_DRIVER" == "" ]; then
-		echo_log "No Wireless Driver Specified"
-		exit
-	fi
-	if [ -n "$WIRELESS_WPAKEY" ] || [ -n "CUSTOM_SUPPLICANT_CONF" ]; then
-		if [ ! -x /sbin/wpa_supplicant ]; then
-			echo_log "Could not find wpa_supplicant"
+	if [ -n "$CUSTOM_SUPPLICANT_CONF" ] ; then
+                wpa_supplicant -B -D`make_lower $WIRELESS_DRIVER` -i$INTERFACE -c$CUSTOM_SUPPLICANT_CONF
+	else
+		if [ "$WIRELESS_ESSID" == "" ]; then
+			echo_log "No ESSID specified"
 			exit
 		fi
-	fi
-	if [ -n "$WIRELESS_WPAKEY" ];  then
-		echo "WIRELESS_WPAKEY=$WIRELESS_WPAKEY" >> /var/log/net/$INTERFACE
-		wpa_passphrase "$WIRELESS_ESSID" "$WIRELESS_WPAKEY" >> /etc/wpa_supplicant.conf.tmp
-		awk '{print $0;if($0=="network={"){print "\teap=TTLS PEAP TLS"}}' < /etc/wpa_supplicant.conf.tmp > /etc/wpa_supplicant.conf.tmp2
-		awk '{print $0;if($0=="network={"){print "\tscan_ssid=1"}}' < /etc/wpa_supplicant.conf.tmp2 > /etc/wpa_supplicant.conf.tmp3
-		awk '{print $0;if($0=="network={"){print "\tkey_mgmt=WPA-EAP WPA-PSK NONE"}}' < /etc/wpa_supplicant.conf.tmp3 > /etc/wpa_supplicant.conf.tmp4
-		awk '{print $0;if($0=="network={"){print "\tpairwise=CCMP TKIP"}}' < /etc/wpa_supplicant.conf.tmp4 > /etc/wpa_supplicant.conf
-		rm -f /etc/wpa_supplicant.conf.tmp*
-		wpa_supplicant -B -D`make_lower $WIRELESS_DRIVER` -i$INTERFACE -c/etc/wpa_supplicant.conf
-		sleep 1
-	elif [ -n "CUSTOM_SUPPLICANT_CONF" ] ; then
-		wpa_supplicant -B -D`make_lower $WIRELESS_DRIVER` -i$INTERFACE -c$CUSTOM_SUPPLICANT_CONF
-	fi
-	# This appears to be a Wireless device (USB/PCI/PCMCIA). Set specific
-	# options. (Code ripped from pcmcia-cs wireless script)
-	# Mode need to be first : some settings apply only in a specific mode !
-	if [ -n "$WIRELESS_MODE" ] ; then
-		iwconfig $INTERFACE mode $WIRELESS_MODE
-	fi
-	# This is a bit hackish, but should do the job right...
-	if [ ! -n "$WIRELESS_NICKNAME" ] ; then
-		WIRELESS_NICKNAME=$CLIENT_NAME
-	fi
-	if [ -n "$WIRELESS_ESSID" -o -n "$WIRELESS_MODE" ] ; then
-		echo "WIRELESS_ESSID=$WIRELESS_ESSID" >> /var/log/net/$INTERFACE
-		iwconfig $INTERFACE nick "$WIRELESS_NICKNAME" >/dev/null 2>&1
-	fi
-	# Regular stuff...
-	if [ -n "$WIRELESS_NWID" ] ; then
-		iwconfig $INTERFACE nwid $WIRELESS_NWID
-	fi
-	if [ -n "$WIRELESS_FREQ" ] ; then
-		iwconfig $INTERFACE freq $WIRELESS_FREQ
-	elif [ -n "$WIRELESS_CHANNEL" ] ; then
-		iwconfig $INTERFACE channel $WIRELESS_CHANNEL
-	fi
-	if [ -n "$WIRELESS_SENS" ] ; then
-		iwconfig $INTERFACE sens $WIRELESS_SENS
-	fi
-	if [ -n "$WIRELESS_RATE" ] ; then
-		iwconfig $INTERFACE rate $WIRELESS_RATE
-	fi
-	if [ -n "$WIRELESS_KEY" ] && [ ! -n "$WIRELESS_WPAKEY" ] ; then
-		echo "WIRELESS_KEY=$WIRELESS_KEY" >> /var/log/net/$INTERFACE
-		iwconfig $INTERFACE key $WIRELESS_KEY
-	fi
-	if [ -n "$WIRELESS_RTS" ] ; then
-		iwconfig $INTERFACE rts $WIRELESS_RTS
-	fi
-	if [ -n "$WIRELESS_FRAG" ] ; then
-		iwconfig $INTERFACE frag $WIRELESS_FRAG
-	fi
-	# More specific parameters
-	if [ -n "$WIRELESS_IWCONFIG" ] ; then
-		iwconfig $INTERFACE $WIRELESS_IWCONFIG
-	fi
-	if [ -n "$WIRELESS_IWSPY" ] ; then
-		iwspy $INTERFACE $WIRELESS_IWSPY
-	fi
-	if [ -n "$WIRELESS_IWPRIV" ] ; then
-		iwpriv $INTERFACE $WIRELESS_IWPRIV
-	fi
-	# ESSID need to be last : most device re-perform the scanning/discovery
-	# when this is set, and things like encryption keys are better be
-	# defined if we want to discover the right set of APs/nodes.
-	if [ -n "$WIRELESS_ESSID" ] && [ ! -n "$WIRELESS_WPAKEY" ] ; then
-		iwconfig $INTERFACE essid "$WIRELESS_ESSID"
+		if [ -n "$WIRELESS_WPAKEY" ] || [ -n "CUSTOM_SUPPLICANT_CONF" ]; then
+			if [ ! -x /sbin/wpa_supplicant ]; then
+				echo_log "Could not find wpa_supplicant"
+				exit
+			fi
+		fi
+		if [ -n "$WIRELESS_WPAKEY" ];  then
+			echo "WIRELESS_WPAKEY=$WIRELESS_WPAKEY" >> /var/log/net/$INTERFACE
+			wpa_passphrase "$WIRELESS_ESSID" "$WIRELESS_WPAKEY" >> /etc/wpa_supplicant.conf.tmp
+			awk '{print $0;if($0=="network={"){print "\teap=TTLS PEAP TLS"}}' < /etc/wpa_supplicant.conf.tmp > /etc/wpa_supplicant.conf.tmp2
+			awk '{print $0;if($0=="network={"){print "\tscan_ssid=1"}}' < /etc/wpa_supplicant.conf.tmp2 > /etc/wpa_supplicant.conf.tmp3
+			awk '{print $0;if($0=="network={"){print "\tkey_mgmt=WPA-EAP WPA-PSK NONE"}}' < /etc/wpa_supplicant.conf.tmp3 > /etc/wpa_supplicant.conf.tmp4
+			awk '{print $0;if($0=="network={"){print "\tpairwise=CCMP TKIP"}}' < /etc/wpa_supplicant.conf.tmp4 > /etc/wpa_supplicant.conf
+			rm -f /etc/wpa_supplicant.conf.tmp*
+			wpa_supplicant -B -D`make_lower $WIRELESS_DRIVER` -i$INTERFACE -c/etc/wpa_supplicant.conf
+			sleep 1
+		fi
+		# This appears to be a Wireless device (USB/PCI/PCMCIA). Set specific
+		# options. (Code ripped from pcmcia-cs wireless script)
+		# Mode need to be first : some settings apply only in a specific mode !
+		if [ -n "$WIRELESS_MODE" ] ; then
+			iwconfig $INTERFACE mode $WIRELESS_MODE
+		fi
+		# This is a bit hackish, but should do the job right...
+		if [ ! -n "$WIRELESS_NICKNAME" ] ; then
+			WIRELESS_NICKNAME=$CLIENT_NAME
+		fi
+		if [ -n "$WIRELESS_ESSID" -o -n "$WIRELESS_MODE" ] ; then
+			echo "WIRELESS_ESSID=$WIRELESS_ESSID" >> /var/log/net/$INTERFACE
+			iwconfig $INTERFACE nick "$WIRELESS_NICKNAME" >/dev/null 2>&1
+		fi
+		# Regular stuff...
+		if [ -n "$WIRELESS_NWID" ] ; then
+			iwconfig $INTERFACE nwid $WIRELESS_NWID
+		fi
+		if [ -n "$WIRELESS_FREQ" ] ; then
+			iwconfig $INTERFACE freq $WIRELESS_FREQ
+		elif [ -n "$WIRELESS_CHANNEL" ] ; then
+			iwconfig $INTERFACE channel $WIRELESS_CHANNEL
+		fi
+		if [ -n "$WIRELESS_SENS" ] ; then
+			iwconfig $INTERFACE sens $WIRELESS_SENS
+		fi
+		if [ -n "$WIRELESS_RATE" ] ; then
+			iwconfig $INTERFACE rate $WIRELESS_RATE
+		fi
+		if [ -n "$WIRELESS_KEY" ] && [ ! -n "$WIRELESS_WPAKEY" ] ; then
+			echo "WIRELESS_KEY=$WIRELESS_KEY" >> /var/log/net/$INTERFACE
+			iwconfig $INTERFACE key $WIRELESS_KEY
+		fi
+		if [ -n "$WIRELESS_RTS" ] ; then
+			iwconfig $INTERFACE rts $WIRELESS_RTS
+		fi
+		if [ -n "$WIRELESS_FRAG" ] ; then
+			iwconfig $INTERFACE frag $WIRELESS_FRAG
+		fi
+		# More specific parameters
+		if [ -n "$WIRELESS_IWCONFIG" ] ; then
+			iwconfig $INTERFACE $WIRELESS_IWCONFIG
+		fi
+		if [ -n "$WIRELESS_IWSPY" ] ; then
+			iwspy $INTERFACE $WIRELESS_IWSPY
+		fi
+		if [ -n "$WIRELESS_IWPRIV" ] ; then
+			iwpriv $INTERFACE $WIRELESS_IWPRIV
+		fi
+		# ESSID need to be last : most device re-perform the scanning/discovery
+		# when this is set, and things like encryption keys are better be
+		# defined if we want to discover the right set of APs/nodes.
+		if [ -n "$WIRELESS_ESSID" ] && [ ! -n "$WIRELESS_WPAKEY" ] ; then
+			iwconfig $INTERFACE essid "$WIRELESS_ESSID"
+		fi
 	fi
 }
 
@@ -269,5 +267,3 @@ main()
 	kernel_settings
 }
 main
-
-
