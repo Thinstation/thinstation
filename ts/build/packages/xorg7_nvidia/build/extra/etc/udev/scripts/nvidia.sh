9ad0cba3cc2b0636e605367f37731374729a539e
Donald A. Cupp Jr <doncuppjr@yahoo.com>
2018-07-23 16:32:57 -0700

    modified:   ts/bin/flash 	modified:   ts/build/build 	modified:   ts/build/conf/development/build.conf.example 	modified:   ts/build/conf/development/thinstation.conf.buildtime 	modified:   ts/build/packages/xorg7_nvidia/build/extra/etc/udev/scripts/nvidia.sh 	modified:   ts/build/packages/xorg7_nvidia/build/install

--- ts/build/packages/xorg7_nvidia/build/extra/etc/udev/scripts/nvidia.sh
+++ ts/build/packages/xorg7_nvidia/build/extra/etc/udev/scripts/nvidia.sh
@@ -1,12 +1,15 @@
 #! /bin/sh
 
-NVGLVER=`find /lib -name libGL.so.\*.\* | cut -d. -f3-`
-
 rm -f /lib/libGL.so.1
-ln -s libGL.so.$NVGLVER /lib/libGL.so.1
-
+ln -s libGL-nvidia		/lib/libGL.so.1
+#rm -f /lib/libGLESv1_CM.so.1
+#ln -s libGLESv1_CM-nvida	/lib/libGLESv1_CM.so.1
+#rm -f /lib/libEGL.so.1
+#ln -s libEGL-nvidia		/lib/libEGL.so.1
+#rm -f /lib/libGLESv2.so.2
+#ln -s libGLESv2-nvidia		/lib/libGLESv2.so.2
 rm -f /lib/xorg/modules/extensions/libglx.so
-ln -s libglx.so.$NVGLVER /lib/xorg/modules/extensions/libglx.so
+ln -s libglx.so-nvidia		/lib/xorg/modules/extensions/libglx.so
 
 cat <<EOF > /etc/X11/xorg.conf.d/20-nvidia.conf
 Section "Device"
