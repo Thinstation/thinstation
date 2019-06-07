f0c3dc028f89f73ea0576641ff2dec2d99307674
Donald A. Cupp Jr <doncuppjr@yahoo.com>
2019-06-07 08:09:31 -0700

    modified:   ts/build/packages/automount/etc/udev/scripts/scsi.sh

--- ts/build/packages/automount/etc/udev/scripts/scsi.sh
+++ ts/build/packages/automount/etc/udev/scripts/scsi.sh
@@ -45,13 +45,13 @@ _unmount()
 		while mounted /dev/$devpath; do
 			mtdpath=`cat /proc/mounts |grep -e /dev/$devpath |tail -n 1 |cut -d ' ' -f 2`
 			systemd-mount -u $mtdpath
-			while [ -n "$mtdpath" ] \
-			   && [ "$mtdpath" != "/mnt" ] \
-			   && [ -z "`ls -A $mtdpath`" ] \
-			   && [ -z "`pidof xfreerdp`" ]; do
-				rmdir $mtdpath
-				mtdpath="`dirname $mtdpath`"
-			done
+#			while [ -n "$mtdpath" ] \
+#			   && [ "$mtdpath" != "/mnt" ] \
+#			   && [ -z "`ls -A $mtdpath`" ] \
+#			   && [ -z "`pidof xfreerdp`" ]; do
+#				rmdir $mtdpath
+#				mtdpath="`dirname $mtdpath`"
+#			done
 		done
 	fi
 }
