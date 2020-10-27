380c8f0d50edd52fee73ef03f10540357912b587
Donald A. Cupp Jr <doncuppjr@yahoo.com>
2020-10-26 21:17:48 -0700

    modified:   ts/build/packages/ica/build/install 	deleted:    ts/ports/nocache/clang/.gitignore 	deleted:    ts/ports/nocache/clang/.md5sum 	deleted:    ts/ports/nocache/clang/Pkgfile 	deleted:    ts/ports/nocache/clang/config.h 	renamed:    ts/ports/nocache/clang/.footprint -> ts/ports/opt/clang/.footprint 	new file:   ts/ports/opt/clang/.md5sum 	new file:   ts/ports/opt/clang/Pkgfile 	renamed:    ts/ports/opt/libclang/libclang#9.0.0-1.pkg.tar.xz -> ts/ports/opt/clang/clang#11.0.0-2.pkg.tar.xz 	renamed:    ts/ports/nocache/clang/llvm-config.h -> ts/ports/opt/clang/llvm-config.h 	deleted:    ts/ports/opt/libclang/.footprint 	deleted:    ts/ports/opt/libclang/.md5sum 	deleted:    ts/ports/opt/libclang/Pkgfile 	modified:   ts/ports/opt/llvm/.footprint 	modified:   ts/ports/opt/llvm/.md5sum 	modified:   ts/ports/opt/llvm/Pkgfile 	renamed:    ts/ports/opt/llvm/llvm#9.0.0-2.pkg.tar.xz -> ts/ports/opt/llvm/llvm#11.0.0-2.pkg.tar.xz 	modified:   ts/ports/opt/qt5/qt5#5.12.4-2.pkg.tar.xz 	modified:   ts/ports/xorg/mesa3d/mesa3d#20.1.3-1.pkg.tar.xz

---
+++ ts/ports/opt/clang/llvm-config.h
@@ -0,0 +1,12 @@
+/* llvm-config.h stub */
+
+#ifndef __STUB__LLVMCONFIG_H__
+#define __STUB__LLVMCONFIG_H__
+
+#if defined(__x86_64__)
+#include "llvm-config-64.h"
+#else
+#include "llvm-config-32.h"
+#endif
+
+#endif
