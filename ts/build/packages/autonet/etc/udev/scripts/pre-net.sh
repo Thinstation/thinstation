8b92ce4507debc858effee887012311d6f5266ac
Donald A. Cupp Jr. <doncuppjr@yahoo.com>
2024-11-22 15:06:12 -0600

    modified:   ts/build/conf/hwlister/build.conf.example 	modified:   ts/build/fastboot/bin-boot 	modified:   ts/build/fastboot/lib-boot 	modified:   ts/build/packages/autonet/.dna 	modified:   ts/build/packages/autonet/etc/init.d/autonet 	modified:   ts/build/packages/autonet/etc/init.d/hostname 	modified:   ts/build/packages/autonet/etc/systemd/system/autonet.service 	modified:   ts/build/packages/autonet/etc/systemd/system/choose-network.service 	modified:   ts/build/packages/autonet/etc/systemd/system/hostname.service 	renamed:    ts/build/packages/autonet/etc/systemd/system/sysinit.target.wants/autonet.service -> ts/build/packages/autonet/etc/systemd/system/multi-user.target.wants/autonet.service 	renamed:    ts/build/packages/autonet/etc/systemd/system/sysinit.target.wants/choose-network.service -> ts/build/packages/autonet/etc/systemd/system/multi-user.target.wants/choose-network.service 	modified:   ts/build/packages/autonet/etc/udev/scripts/lease_dhcp 	new file:   ts/build/packages/autonet/etc/udev/scripts/pre-net.sh 	renamed:    ts/build/packages/systemd/build/extra/etc/systemd/system/systemd-resolved.service.d/overrides.conf -> ts/build/packages/avahi/build/extra/etc/systemd/system/avahi-daemon.service.d/overrides.conf 	modified:   ts/build/packages/base/etc/init.d/fastboot 	modified:   ts/build/packages/base/etc/systemd/system/crond.service 	modified:   ts/build/packages/base/etc/systemd/system/crontab.service 	modified:   ts/build/packages/base/etc/systemd/system/display-manager.service 	modified:   ts/build/packages/base/etc/systemd/system/fastboot.service 	renamed:    ts/build/packages/base/etc/systemd/system/sysinit.target.wants/fastboot.service -> ts/build/packages/base/etc/systemd/system/multi-user.target.wants/fastboot.service 	modified:   ts/build/packages/base/etc/systemd/system/pkg.service 	modified:   ts/build/packages/base/etc/systemd/system/session.service 	modified:   ts/build/packages/base/etc/systemd/system/vmtouch.service 	deleted:    ts/build/packages/base/etc/udev/scripts/pre-net.sh 	new file:   ts/build/packages/bluetooth/build/extra/etc/systemd/system/blueman-mechanism.service.d/overrides.conf 	new file:   ts/build/packages/cups/build/extra/etc/systemd/system/cups.socket.d/overrides.conf 	new file:   ts/build/packages/lighttpd/build/extra/etc/systemd/system/lighttpd.service.d/overrides.conf 	modified:   ts/build/packages/networkmanager/build/extra/etc/NetworkManager/dispatcher.d/00-update-status 	modified:   ts/build/packages/networkmanager/build/extra/etc/init.d/nmcreatesysconn 	modified:   ts/build/packages/networkmanager/build/finalize 	modified:   ts/build/packages/nftables/build/extra/etc/systemd/system/nftables.service.d/overrides.conf 	modified:   ts/build/packages/spice-vdagent/build/extra/etc/systemd/system/9p-kvm-mounts.service 	modified:   ts/build/packages/spice-vdagent/build/extra/etc/systemd/system/9p-qemu-mounts.service 	renamed:    ts/build/packages/spice-vdagent/build/extra/etc/systemd/system/local-fs.target.wants/9p-kvm-mounts.service -> ts/build/packages/spice-vdagent/build/extra/etc/systemd/system/multi-user.target.wants/9p-kvm-mounts.service 	renamed:    ts/build/packages/spice-vdagent/build/extra/etc/systemd/system/local-fs.target.wants/9p-qemu-mounts.service -> ts/build/packages/spice-vdagent/build/extra/etc/systemd/system/multi-user.target.wants/9p-qemu-mounts.service 	deleted:    ts/build/packages/systemd/build/extra/etc/systemd/system/systemd-vconsole-setup.service.d/overrides.conf 	modified:   ts/unwind_cache/autonet.turbo 	modified:   ts/wind_cache/autonet.turbo

---
+++ ts/build/packages/autonet/etc/udev/scripts/pre-net.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+        set -x
+	exec </dev/null >>/var/log/pre-net.log  2>&1
+
+env |grep -v "^ID" > /var/log/net/$INTERFACE
+
+if [ "$(systemctl is-system-running)" = "initializing" ]; then
+    echo "Systemd is in the boot phase."
+else
+	export INTERFACE
+	/etc/udev/scripts/net.sh
+fi
--- ts/build/packages/base/etc/udev/scripts/pre-net.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set > /var/log/net/$INTERFACE
-if [ ! -e /tmp/init ]; then
-	export INTERFACE
-	/etc/udev/scripts/net.sh
-fi
